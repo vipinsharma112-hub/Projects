6bd7cb4abfdfb41bc3bf691541e7407a5100a453
vipin sharma <vipin.sharma112@gmail.com>
2026-07-22 11:27:01 +0000

    StreamTest1

---
+++ JAVA/StreamTest1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.util.stream.*;
+public class StreamTest1{
+	public static void main(String[] args){
+		List<String> strings=List.of("I","am","a","list","of","Strings");
+		List<String> result=strings.stream().sorted((s1,s2)->s1.compareToIgnoreCase(s2)).skip(2).limit(4).collect(Collectors.toList());
+		System.out.println("result="+result);
+	}
+}
