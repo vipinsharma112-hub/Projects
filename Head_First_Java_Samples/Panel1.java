fae11c4463b96261be74d5dc53698211b4bdff40
Vipin Sharma <vipin.sharma112@gmail.com>
2026-09-15 15:47:06 +0530

    created a program Square.java for saving object making ObjectOutputStream chained to the connection stream java.io package and implements serializable

--- Head_First_Java_Samples/Panel1.java
+++ Head_First_Java_Samples/Panel1.java
@@ -9,6 +9,15 @@ public class Panel1 {
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.darkGray);
+
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+		JButton button = new JButton("shock me");
+		JButton buttonTwo = new JButton("bliss");
+		JButton buttonThree = new JButton("huh?");
+		panel.add(button);
+		panel.add(buttonTwo);
+		panel.add(buttonThree);
 		frame.getContentPane().add(BorderLayout.EAST, panel);
 		frame.setSize(200, 200);
 		frame.setVisible(true);
