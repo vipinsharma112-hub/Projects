cfe3f4ae3c2464fe2e10885418f1bec4c43dd363
Vipin Sharma <vipin.sharma112@gmail.com>
2026-09-24 23:33:18 +0530

    created a program named Person.kt in Kotlin a example

---
+++ Kotlin_In_Action/Person.kt
@@ -0,0 +1,15 @@
+data class Person(
+    val name: String,
+    val age: Int? = null
+)
+
+ fun main() {
+      val persons = listOf(
+       Person("Alice", age=29),
+       Person("Bob"),
+)
+    val oldest = persons.maxBy {
+       it.age ?: 0
+}
+    println("The oldest is: $oldest")
+}
